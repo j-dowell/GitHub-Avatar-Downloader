285dd700dcf8701d4b2c31a00d91eff2c83228e2
James Dowell <james.s.dowell@gmail.com>
2018-08-07 01:35:39 +0000

    added comments

--- download_avatars.js
+++ download_avatars.js
@@ -8,13 +8,14 @@ if (process.argv.length === 4) {
   var inputOwner = process.argv[2];
   var inputName = process.argv[3];
 } else {
-  console.log("Please enter repository owner and name");
+  console.log("Please enter valid repository owner and name");
   return;
 }
 
 // Retrieves contributor avatar links based on user input
 function getRepoContributors(repoOwner, repoName, cb) {
   console.log('Welcome to the GitHub Avatar Downloader!');
+
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -24,8 +25,9 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    cb(err, body); // Invoking getContributorList as callback function
   });
+
 }
 
 // Creates a list of contributor avatars to pass to downloadImageByURL function
@@ -38,11 +40,11 @@ function getContributorList(err, result) {
 
   contributorsList.forEach(function(item) {
     var path = `avatars/${item.login}.jpg`;
-    downloadImageByURL(item.avatar_url, path);
+    downloadImageByURL(item.avatar_url, path); // Looping through each contributor and invoking download image function
   });
 }
 
-// Takes in URL and downloads images to file path
+// Takes in URL and downloads images to specified file path, assuming folder exists
 function downloadImageByURL(url, filePath) {
   request.get(url)
           .on('error', function(err) {
@@ -50,7 +52,7 @@ function downloadImageByURL(url, filePath) {
           })
           .pipe(fs.createWriteStream(filePath))
           .on('finish', function() {
-            console.log('Downloaded!');
+            console.log('Image Downloaded');
           });
 }
 
